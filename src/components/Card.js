1a918de7ff4b4f8c437ed3380c33d54c7a69bd96
Unknown <joeyskuo@gmail.com>
2017-10-01 17:20:20 -0700

    added basic card behavior

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,14 @@ import '../css/_card.css';
 
 class Card extends Component {
 
+    postVote() {
+      console.log("post request sent!");
+    }
+
     render() {
 
         return (
-            <div className="card">
+            <div className="card" onClick={() => this.postVote()}>
               <p>Test Card</p>
             </div>
         )
